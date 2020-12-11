b2753a96230c075491369aa5959ff6e2ecdfb390
Guillaume <Guillaume_Fray@hotmail.fr>
2020-12-11 11:41:03 +0000

    11/12/20

--- Test.m
+++ Test.m
@@ -10,7 +10,7 @@ labels = zeros(k,10);
 for i = 1:size(labels_origin(:,:))
     tget = labels_origin(i,1);
     for j = 1:10
-        if j == tget
+        if j == tget+1
             labels(i,j) = 1;
         else
             labels(i,j) = 0;
@@ -37,6 +37,11 @@ m.initWeights(1.0);
 % 60,000 outputs. Each output is an array of 10 binary values
 outputs = zeros(k,10);
 
+
+num_input = 50;
+
+
+
 tic
 
 for x=1:10000 % 10000
@@ -44,11 +49,11 @@ for x=1:10000 % 10000
     if mod(x,1000) == 0
         fprintf('x is at %i \n', x);% 
     end
-    for i = 1:50 %k
+    for i = 1:num_input %k
         if mod(i,1000) == 0
             fprintf('           image --> %i \n', i);% 
         end  
-        m.adapt_to_target(images(:,i), labels(:,i), 1.7); %%%%%%%%%%%%%%%
+        m.adapt_to_target(images(:,i), labels(:,i), 0.3); %%%%%%%%%%%%%%%
         target = labels(:,i);
         o = m.compute_output(images(:,i));
         outputs(i,:) = o;
@@ -59,30 +64,51 @@ end
 
 %%%%%%%%-------------------%%%%%%%%%%%
 disp('----- Targets -----');
-display_network(images(:,1:50));
-disp('labels(:,1:50)');
-disp(labels(:,1:50));
-disp('----- Outputs -----');
-disp(outputs(1:50,:));
+display_network(images(:,1:num_input));
+disp('labels(:,1:num_input)');
+disp(labels(:,1:num_input));
+% disp('----- Outputs -----');
+% disp(outputs(1:num_input,:));
 
 undetermined = 0; %Counter
-for i = 1:50
+correct = 0; %Counter
+incorrect = 0; %Counter
+for i = 1:num_input
     determined = 0; %Boolean
     for j = 1:10
-        if outputs(i,j) > 0.9
-            fprintf('Output %i is %i \n', i, j);%
+        if outputs(i,j) > 0.9 && outputs(i,j) == max(outputs(i,j))
+            fprintf('Output %i is %i \n', i, j-1);%
             determined = 1;
+            fprintf('Label %i is %i \n', i, labels_origin(i,1));%
+            if j-1 == labels_origin(i,1)
+                correct = correct + 1;
+            else
+                incorrect = incorrect + 1;
+            end
         elseif determined == 0 && j == 10
-            fprintf('Output %i could not be determined \n', i);% 
+%             fprintf('Output %i could not be determined \n', i);% 
             undetermined = undetermined + 1;
         end
     end
 end    
 
+disp('\n \n');%
 fprintf('Number of undetermined outputs are %i \n', undetermined);% 
+fprintf('Number of correct outputs are %i \n', correct);% 
+fprintf('Number of incorrect outputs are %i \n', incorrect);% 
+
+
 
 
 toc
+
+
+
+
+
+
+
+
 
 
 
